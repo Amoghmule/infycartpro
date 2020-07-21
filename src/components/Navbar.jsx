e0654a5744a2cbd8a30f4cc2be53ba1ae1294b7d
Amoghmule <amoghmule13@gmail.com>
2020-07-21 18:49:00 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,6 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar  navbar-expand-md navbar-dark bg-dark">
-       
         <a href="#" className="navbar-brand">
           Chocolate Factory
         </a>
@@ -15,13 +14,12 @@ class Navbar extends Component {
           type="button"
           data-toggle="collapse"
           data-target="#navbarNav"
-          
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        
+
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav text-center">
             <li className="nav-item active">
               <a className="nav-link" href="#">
                 Home <span className="sr-only">(current)</span>
@@ -29,16 +27,15 @@ class Navbar extends Component {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Cart
+                Cart{" "}
+                <i class="fa fa-envelope-o">
+                  <span class="badge badge-primary">11</span>
+                </i>
               </a>
             </li>
-            
           </ul>
         </div>
-        
       </nav>
-      
-      
     );
   }
 }
