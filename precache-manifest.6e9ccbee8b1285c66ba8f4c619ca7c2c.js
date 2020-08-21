c1e1ee81e9df971415c34f18b523e60acb165d64
Amoghmule <amoghmule13@gmail.com>
2020-08-21 19:55:06 +0530

    Updates

---
+++ precache-manifest.6e9ccbee8b1285c66ba8f4c619ca7c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01de7f803ea404eadbf71f66f633decd",
+    "url": "/infycartpro/index.html"
+  },
+  {
+    "revision": "184d7558b0c8b63f417c",
+    "url": "/infycartpro/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "73739dc6ccce01349464",
+    "url": "/infycartpro/static/css/main.ac233900.chunk.css"
+  },
+  {
+    "revision": "184d7558b0c8b63f417c",
+    "url": "/infycartpro/static/js/2.f8b5057d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/infycartpro/static/js/2.f8b5057d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73739dc6ccce01349464",
+    "url": "/infycartpro/static/js/main.76add5b0.chunk.js"
+  },
+  {
+    "revision": "110b2754be1465747f57",
+    "url": "/infycartpro/static/js/runtime-main.a81b4c02.js"
+  }
+]);
